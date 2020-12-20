aab8b78970fdc3c467a9b625b50b0eb29e2c953c
sinaFallahpour <fallahpour.sina77@gmail.com>
2020-12-20 10:53:34 +0330

    some changes

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,6 +1,13 @@
 const CACHE_NAME = "version-1";
-const urlsToChache = ["index.html", "offline.html", "./static/js/main.chunk.js",
-  "./css/main.css", "./css/spacing.min.css", "./static/js/0.chunk.js"];
+// const urlsToChache = ["index.html", "offline.html", "./static/js/main.chunk.js",
+//   "./css/main.css", "./css/spacing.min.css", "./static/js/0.chunk.js"];
+
+const urlsToChache = ["./index.html", "./offline.html",
+  "./css/main.css", "./css/spacing.min.css",
+  "./static/js/main.ef6d7cd0.chunk.js", "./static/js/2.26da0ab9.chunk.js", "./static/js/runtime-main.7f7134f8.js"
+];
+
+
 
 const self = this;
 
@@ -17,7 +24,7 @@ self.addEventListener("install", (event) => {
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then(() => {
-      return fetch(event.request).catch(() => caches.match("offline.html"));
+      return fetch(event.request).catch(() => caches.match(urlsToChache));
     })
   );
 });
